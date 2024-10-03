0b74d0fd97dee7ad84d473e75e7f610e30c935f9
Nguyễn Đình Trường <ndtr2000@gmail.com>
2024-10-03 16:39:01 +0700

    change script to use private key

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,9 +4,10 @@ pragma solidity >=0.8.25 <0.9.0;
 import { Foo } from "../src/Foo.sol";
 
 import { BaseScript } from "./Base.s.sol";
+import { Constant } from "./Constant.s.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
-contract Deploy is BaseScript {
+contract Deploy is BaseScript, Constant {
     function run() public broadcast returns (Foo foo) {
         foo = new Foo();
     }
